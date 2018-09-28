9801eb0e420baae971a58cb1afad975b3d6fa1b2
Jose Manuel Rufo Delgado <jmrufo@gmail.com>
2018-09-28 16:36:11 +0200

    Continuación desarrollo comprobación login

--- login/valida_login.php
+++ login/valida_login.php
@@ -2,10 +2,21 @@
 
     require("../src/conexionDB.class.php");
 
+
+        $usuario = $_POST['username'];
+        $clave = $_POST['pass'];
+
+        echo "Usuario: " . $usuario . "<br> Clave: " . $clave;
+
+
+
+
     /**
     * Establecemos conexión con DBase
     **/
     $dbconecta = new conexionDB();
+
+
 
 
 
