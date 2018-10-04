71d0331d32047b3605c18f750edc12691a3d41c3
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-04 11:44:57 +0200

    Creación de include para Pie página

---
+++ inc/footer.inc.php
@@ -0,0 +1,11 @@
+<div id="pie_pagina">	
+	<div class="bdorado"></div>
+	<div class="content_pie">
+		<div class="info_pie">
+			<img src="../lib/images/logos/footer-logo.png" alt="Talention">
+			<span style="margin:0px 10px 0px 10px; font-size:18px; vertical-align:middle;"> | </span>
+			<img src="../lib/images/icons/ico_lugar.png" alt="Icono Dirección">
+			<span>Avda. Príncipe de Asturias, nº 9 - 41.500 - Alcalá de Guadaira (Sevilla)</span>
+		</div>
+	</div>
+</div>
