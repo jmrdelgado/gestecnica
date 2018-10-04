346df9697924ff6d00db27b06d2b0d66dce61f56
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-04 14:48:51 +0200

    Ajuste estilo texto pie

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -3,7 +3,7 @@
 	<div class="content_pie">
 		<div class="info_pie">
 			<img src="../lib/images/logos/footer-logo.png" alt="Talention">
-			<span style="margin:0px 10px 0px 10px; font-size:18px; vertical-align:middle;"> | </span>
+			<span style="margin:0px 10px 0px 10px; font-size:16px; vertical-align:middle;"> | </span>
 			<img src="../lib/images/icons/ico_lugar.png" alt="Icono Dirección">
 			<span>Avda. Príncipe de Asturias, nº 9 - 41.500 - Alcalá de Guadaira (Sevilla)</span>
 		</div>
