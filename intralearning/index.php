dd73af605d9f1318cf0885928a9fd44a17ff095e
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-05 14:55:17 +0200

    Codificación página que deriva a administrador y cliente

--- intralearning/index.php
+++ intralearning/index.php
@@ -5,15 +5,23 @@
      */
     session_start();
     
-    if (!isset($_SESSION['usuario'])) {
+    if (isset($_SESSION['usuario']) && $_COOKIE['perfil_user'] == "administrador") {
         
-        header("location:../index.php");
+        require_once("layout_admin.php");
         
     } else {
         
-        echo "Usuario logeado: " . $_SESSION['usuario'];
-        
-        echo "<br><br><a href='../login/close_sesion.php' name='Cerrar Sesión'>Cerrar Sesión</a>";
+        if (isset($_SESSION['usuario']) && $_COOKIE['perfil_user'] == "gestor") {
+         
+            echo "Usuario logeado: " . $_SESSION['usuario'];
+            
+            echo "<br><br><a href='../login/close_sesion.php' name='Cerrar Sesión'>Cerrar Sesión</a>";
+            
+        } else {
+            
+            header("location:../index.php");
+            
+        }
         
     }
 
