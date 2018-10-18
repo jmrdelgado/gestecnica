066838fd641c9b1f20e63e950ecaddd5ca0d21ea
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-18 12:40:35 +0200

    Lógica de funcionamiento para el alta de acciones y reubicación de validación de logeo

---
+++ logic/alta_new_accion.php
@@ -0,0 +1,70 @@
+<?php
+
+    require '../src/ConsultaCliente.class.php';
+    
+    /**
+     * Obtenemos Id de cliente conectado
+     * @var ConsultaCliente $idcliente
+     */
+    $idcliente = new ConsultaCliente();
+    $id = $idcliente->getIdCliente($_COOKIE['name_user']);
+    
+    /**
+     * Establecemos conexión con DBase
+     * @var ConexionDB $dbconecta
+     */
+    $dbconn = $idcliente->getconectPDO();
+    
+    /**
+     * Consulta de insercción de acciones
+     * @var string $sql_insert
+     */
+    $sql_insert = "INSERT INTO acciones (naccion, denominacion, nhoras, modalidad, objetivos, contenidos, id_cliente)
+                    VALUES (:c_accion,:c_denominacion,:c_horas,:c_modalidad,:c_objetivos,:c_contenidos,$id)";
+    
+    
+    $accion = htmlentities(addslashes($_POST['naccion']));
+    $denominacion = addslashes($_POST['denominacion']);
+    $horas = htmlentities(addslashes($_POST['nhoras']));
+    $modalidad = addslashes($_POST['modalidad']);
+    $objetivos = addslashes($_POST['objetivos']);
+    $contenidos = addslashes($_POST['contenidos']);
+    
+    try {
+        $stmt = $dbconn->prepare($sql_insert);
+        $stmt->bindValue(':c_accion', $accion);
+        $stmt->bindValue(':c_denominacion', $denominacion);
+        $stmt->bindValue(':c_horas', $horas);
+        $stmt->bindValue(':c_modalidad', $modalidad);
+        $stmt->bindValue(':c_objetivos', $objetivos);
+        $stmt->bindValue(':c_contenidos', $contenidos);
+        $stmt->execute();   
+    
+        $numreg = $stmt->rowCount();
+        
+        if ($numreg != 0) {
+            
+            header("location:../intralearning/clientes/layout_client.php?p=confirmed");
+            
+        }
+        
+    } catch (PDOException $e) {
+        
+        if ($e->getCode() == 23000) {
+            echo "Error en el proceso de alta.<br>";
+            echo "La acción <strong>" . $accion . "</strong> ya existe en el sistema.<br>";
+            echo "Indique otro número de acción<br><br>";
+            echo "<a href='../intralearning/clientes/layout_client.php?p=new_accion'>Aceptar</a>";
+        } else {
+            echo "Error en el proceso de alta.<br>";
+            echo "Póngase en contacto con el administrador del sistema";
+        }
+    }        
+        
+    /**
+     * Liberamos memoria y cerramos conexión
+     **/
+    $stmt->closeCursor();
+    $dbconn = null;
+
+?>
---
+++ logic/valida_login.php
@@ -0,0 +1,64 @@
+<?php
+
+    require("../src/ConexionDB.class.php");
+
+    /**
+    * Establecemos conexión con DBase y comprobamos datos de usuario
+    **/
+    $dbconn = new ConexionDB();
+    $dbconn = $dbconn->getconectPDO();
+
+    $sql = "SELECT * FROM usuarios WHERE user = :c_user AND password = :c_pass AND suspendido = 0";
+
+    /**
+    * Obtenemos datos introducidos por usuario
+    **/
+    $user = htmlentities(addslashes($_POST['username']));
+    $psw = htmlentities(addslashes($_POST['pass']));
+
+    /**
+    * Preparamos consulta y obtenemos datos
+    **/
+    $stmt = $dbconn->prepare($sql);
+    $stmt->bindValue(':c_user', $user);
+    $stmt->bindValue(':c_pass', $psw);
+    $stmt->execute();
+
+    $numreg = $stmt->rowCount();
+
+    if ($numreg != 0) {      
+
+        /**
+        * Obtenemos datos del usuario logado
+        **/
+        $infouser = $stmt->fetch(PDO::FETCH_ASSOC);
+        $rol = $infouser['permisos'];
+        
+        /**
+         * Iniciamos control de la sesión de usuario
+         **/
+        session_start();
+        $_SESSION['rol_user'] = $rol;
+        
+        /**
+         * Creamos cookie con nombre usuario
+         */
+        setcookie("name_user", $user, time()+86400, "/gestecnica");
+
+        header("location:../login/acceso_usuarios.php");
+
+
+    } else {
+
+        header("location:../index.php");
+        exit();
+
+    }
+
+    /**
+    * Liberamos memoria y cerramos conexión
+    **/
+    $stmt->closeCursor();
+    $dbconn = null;
+
+?>
