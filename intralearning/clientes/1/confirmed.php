c987756bb3e6187a9a0460f2510df55d1bac4497
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-25 09:05:28 +0200

    Creación página confirmación de altas

--- intralearning/clientes/1/confirmed.php
+++ intralearning/clientes/1/confirmed.php
@@ -9,10 +9,25 @@ if (!$_SESSION['rol_user'] == "cliente") {
 
 ?>
 
-<div class="row">
-	<div class="listado-cab-title"></div>
- 	<div class="confirmed">
-     	<p style='text-align: center; padding: 50px 0px 20px 0px; font-size: 18px;'>Proceso de alta finalizado con éxito</p>
-    	<a href="../clientes/layout_client.php?p=new_accion"><button class="btn btn-success w-50" type="button" style="cursor:pointer; margin:0 auto;">Aceptar</button></a>
+<div class="content_form">
+    <div class="row">
+    	<div class="listado-cab-title">¡¡AVISO DE CONFIRMACIÓN!!</div>
+     	<div class="confirmed">
+         	<p style='text-align: center; padding: 50px 0px 20px 0px; font-size: 18px;'>Proceso de alta finalizado con éxito</p>
+         	<?php 
+         	
+             	if ($_GET['tp'] == "tt") {
+             	    
+             	    echo "<a href='../clientes/layout_client.php?p=new_tutor'><button class='btn btn-success w-50' type='button' style='cursor:pointer; margin:0 auto;'>Aceptar</button></a>";
+             	    
+             	} elseif ($_GET['tp'] == "ac") {
+             	    
+             	    echo "<a href='../clientes/layout_client.php?p=new_accion'><button class='btn btn-success w-50' type='button' style='cursor:pointer; margin:0 auto;'>Aceptar</button></a>";
+             	
+             	}
+         	
+         	?>
+        	
+        </div>
     </div>
 </div>
