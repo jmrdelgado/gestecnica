b0b2e14459d48ec73bd070e81e701a840210de30
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-25 12:43:48 +0200

    Corrección etiquetas campos formulario

--- intralearning/clientes/1/new_grupo.php
+++ intralearning/clientes/1/new_grupo.php
@@ -16,10 +16,10 @@ require_once '../../src/ConsultaCliente.class.php';
     	<div class="listado-cab-title">ALTA DE GRUPOS FORMATIVOS</div>
     	
     	<!-- <form class="form-acciones validate-form" method="post" action="" name="alta_acciones"> -->
-    	<form class="form-acciones needs-validation was-validated" method="post" action="" name="alta_grupos">	  
+    	<form class="form-acciones needs-validation was-validated" method="post" action="../../logic/alta_new_grupo.php" name="alta_grupos">	  
             <div class="mb-3">
-            	<label for="naccion">Acción Formativa</label>
-                    <select class="custom-select d-block w-50" id="naccion" required>
+            	<label for="denominacion">Acción Formativa</label>
+                    <select class="custom-select d-block w-50" id="denominacion" name="denominacion" required>
                     	<option value="">Seleccione una acción formativa</option>
                         <?php 
                             
@@ -28,7 +28,7 @@ require_once '../../src/ConsultaCliente.class.php';
                             $rst_acciones = $consultar->getAcciones($id_c);
                             
                             foreach ($rst_acciones as $curso) {
-                                echo '<option value="' . $curso->denominacion . '">' . $curso . '</option>';
+                                echo '<option value="' . $curso . '">' . $curso . '</option>';
                             }
                                             
                         ?>
@@ -38,22 +38,22 @@ require_once '../../src/ConsultaCliente.class.php';
             <div class="row">
                 <div class="col">
                 	<label for="ngrupo">Nº Grupo*</label>
-                    <input type="text" class="form-control" id="ngrupo" placeholder="" pattern="[0-9]{5}" maxlength="5" required>
+                    <input type="text" class="form-control" id="ngrupo" name="ngrupo" placeholder="" pattern="[0-9]{1-5}" maxlength="5" required>
                 </div>
                 
                 <div class="col">
                     <label for="finicio">Fecha Inicio*</label>
-                    <input type="date" class="form-control" id="finicio" placeholder="" required>
+                    <input type="date" class="form-control" id="finicio" name="finicio" placeholder="" required>
                 </div>
                 
                 <div class="col">
                     <label for="ffin">Fecha Fin*</label>
-                    <input type="date" class="form-control" id="ffin" placeholder="" required>
+                    <input type="date" class="form-control" id="ffin" name="ffin" placeholder="" required>
                 </div>
                 
                 <div class="col">
                 	<label for="nalumnos">Nº Participantes</label>
-                    <input type="text" class="form-control" id="nalumnos" pattern="[0-9]{3}" maxlength="3" placeholder="">
+                    <input type="text" class="form-control" id="nalumnos" name="nalumnos" pattern="[0-9]{1-3}" maxlength="3" placeholder="">
                 </div>
     		</div>
     		
@@ -62,12 +62,12 @@ require_once '../../src/ConsultaCliente.class.php';
     		<div>
     		    <div class="col-sm-3">
                 	<label for="cif_centro">CIF</label>
-                    <input type="text" class="form-control" id="cif_centro" pattern="^[A-Wa-w]{1}[0-9]{8}" maxlength="9" placeholder="">
+                    <input type="text" class="form-control" id="cif_centro" name="cif_centro" pattern="^[A-Wa-w]{1}[0-9]{8}" maxlength="9" placeholder="">
                 </div>
                 
                 <div class="col">
-                	<label for="centro_formacion">Centro Formación</label>
-                    <input type="text" class="form-control" id="centro_formacion" placeholder="">
+                	<label for="name_centro">Centro Formación</label>
+                    <input type="text" class="form-control" id="name_centro" name="name_centro" placeholder="">
                 </div>
             </div>
             
@@ -76,17 +76,17 @@ require_once '../../src/ConsultaCliente.class.php';
             <div class="row">
                 <div class="col">
                 	<label for="url_plataforma">URL Plataforma</label>
-                    <input type="url" class="form-control col-lg-auto" id="url_plataforma" placeholder="">
+                    <input type="url" class="form-control col-lg-auto" id="url_plataforma" name="url_plataforma" placeholder="">
                 </div>
                 
                 <div class="col-sm-3">
-                	<label for="usuario_plataforma">Usuario</label>
-                    <input type="text" class="form-control col-sm-auto" id="usuario_plataforma" placeholder="">
+                	<label for="user_plataforma">Usuario</label>
+                    <input type="text" class="form-control col-sm-auto" id="user_plataforma" name="user_plataforma" placeholder="">
                 </div>
                 
                 <div class="col-sm-3">
-                	<label for="clave_plataforma">Clave</label>
-                    <input type="password" class="form-control col-sm-auto" id="clave_plataforma" placeholder="">
+                	<label for="pass_plataforma">Clave</label>
+                    <input type="password" class="form-control col-sm-auto" id="pass_plataforma" name="pass_plataforma" placeholder="">
                 </div>
             </div>
             
@@ -95,16 +95,16 @@ require_once '../../src/ConsultaCliente.class.php';
 			<div class="row">
 				<div class="col">
                     <label for="horario">Horario Mañana</label>
-                    <input type="text" class="form-control col-sm-9" id="hora_m_de" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
+                    <input type="text" class="form-control col-sm-9" id="hora_m_de" name="hora_m_de" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
                     <label style="text-align: center; width: 150px;">a</label>
-					<input type="text" class="form-control col-sm-9" id="hora_m_a" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
+					<input type="text" class="form-control col-sm-9" id="hora_m_a" name="hora_m_a" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
 				</div>
 
 				<div class="col">
                     <label for="horario">Horario Tarde</label>
-                    <input type="text" class="form-control col-sm-9" id="hora_t_de" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
+                    <input type="text" class="form-control col-sm-9" id="hora_t_de" name="hora_t_de" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
 					<label style="text-align: center; width: 150px;">a</label>
-					<input type="text" class="form-control col-sm-9" id="hora_t_a" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
+					<input type="text" class="form-control col-sm-9" id="hora_t_a" name="hora_t_a" placeholder="00:00" pattern="[0-9]{2}[:][0-9]{2}" maxlength="5" required>
 				</div>
 				
 				<div class="col">
@@ -114,32 +114,32 @@ require_once '../../src/ConsultaCliente.class.php';
 					
 					<div class="row">
     					<div class="form-control col-sm-2">
-        					<label>L</label>
-        					<input type="checkbox" class="form-control" name="lunes" value="true">
+        					<label for="lunes">L</label>
+        					<input type="checkbox" class="form-control" id="lunes" name="lunes" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>M</label>
-        					<input type="checkbox" class="form-control" name="martes" value="true">
+        					<label for="martes">M</label>
+        					<input type="checkbox" class="form-control" id="martes" name="martes" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>X</label>
-        					<input type="checkbox" class="form-control" name="miércoles" value="true">
+        					<label for="miercoles">X</label>
+        					<input type="checkbox" class="form-control" id="miercoles" name="miercoles" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>J</label>
-        					<input type="checkbox" class="form-control" name="jueves" value="true">
+        					<label for="jueves">J</label>
+        					<input type="checkbox" class="form-control" id="jueves" name="jueves" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>V</label>
-        					<input type="checkbox" class="form-control" name="viernes" value="true">
+        					<label for="viernes">V</label>
+        					<input type="checkbox" class="form-control" id="viernes" name="viernes" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>S</label>
-        					<input type="checkbox" class="form-control" name="sábado" value="true">
+        					<label for="sabado">S</label>
+        					<input type="checkbox" class="form-control" id="sabado" name="sabado" value="1">
         				</div>
         				<div class="form-control col-sm-2">
-        					<label>D</label>
-        					<input type="checkbox" class="form-control" name="domingo" value="true">
+        					<label for="domingo">D</label>
+        					<input type="checkbox" class="form-control" id="domingo" name="domingo" value="1">
         				</div>
     				</div>
 				</div>
@@ -149,7 +149,7 @@ require_once '../../src/ConsultaCliente.class.php';
 			
 			<div class="mb-3">
             	<label for="ntutor">Tutor / Docente</label>
-                    <select class="custom-select d-block w-50" id="ntutor" required>
+                    <select class="custom-select d-block w-50" id="ntutor" name="ntutor" required>
                         <option value="">Seleccione un Tutor/Docente</option>
     						<?php 
                                 
