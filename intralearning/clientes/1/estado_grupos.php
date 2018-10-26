7e890db5354eff78f387037705f5e65a104aa61d
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2018-10-26 14:36:27 +0200

    Creación rejilla para mostrar datos de grupos por cliente

--- intralearning/clientes/1/estado_grupos.php
+++ intralearning/clientes/1/estado_grupos.php
@@ -6,11 +6,33 @@
     if (!$_SESSION['rol_user'] == "cliente") {
         header("location:../index.php");
     }
+    
+    require_once '../../src/ConsultaCliente.class.php';
 ?>
 
 <div class="content_form">
     <div class="row">
     	<div class="listado-cab-title">ESTADO DE GRUPOS FORMATIVOS COMUNICADOS</div>
-    	
+    	<table style="width: 100%; margin: 10px;">
+        	<tr>
+        		<td>ACCIÓN</td><td>GRUPO</td><td>DENOMINACIÓN</td><td style="text-align:center;">F.INICIO</td><td style="text-align:center;">F.FIN</td><td>Nº ALUMNOS</td><td>ESTADO</td>
+        	</tr>
+        	
+        	<?php
+        	
+        	   $con_grupos = new ConsultaCliente();
+        	   $id_c = $con_grupos->getIdCliente($_COOKIE['name_user']);
+        	   $rst_grupos = $con_grupos->getGruposCliente($id_c);
+        	   
+        	   foreach ($rst_grupos as $grupo) {
+        	       echo '<tr style="font-size:12px;">';
+        	       echo '<td style="text-align:center;">' . $grupo->naccion . '</td><td style="text-align:center;">' . $grupo->ngrupo . '</td><td>' . $grupo->denominacion . '</td>';
+        	       echo '<td style="text-align:center;">' . $grupo->finicio . '</td><td style="text-align:center;">' . $grupo->ffin . '</td"><td style="text-align:center;">' . $grupo->nalumnos . '</td>';
+        	       echo '<td>' . $grupo->estado .'</td>';
+        	       echo '</tr>';
+        	   }
+
+            ?>
+    	</table>
     </div>
 </div>
